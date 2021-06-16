4eda48d11d6d63ddfc67bd680547defb5014919c
samet-kemal <77715935+samet-kemal@users.noreply.github.com>
2021-06-16 22:10:26 +0300

    FeedActivity edited

--- app/src/main/java/com/samet/kotlinfirebaseinstagramclone/FeedActivity.kt
+++ app/src/main/java/com/samet/kotlinfirebaseinstagramclone/FeedActivity.kt
@@ -1,26 +1,30 @@
 package com.samet.kotlinfirebaseinstagramclone
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class FeedActivity : AppCompatActivity() {
 
 
-    private lateinit var  auth:FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
 
         val menuInflater = menuInflater
-        menuInflater.inflate(R.menu.options_menu,menu)
+        menuInflater.inflate(R.menu.options_menu, menu)
 
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.add_post){
+        if (item.itemId == R.id.add_post) {
             //UPload Activity
             val intent = Intent(applicationContext,UploadActivity::class.java)
             startActivity(intent)
@@ -40,5 +44,38 @@ class FeedActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed)
         auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+
+        getDataFromFireStore()
     }
+
+    fun getDataFromFireStore() {
+
+        db.collection("Posts").addSnapshotListener { snapshot, exception ->
+            if (exception != null) {
+                Toast.makeText(
+                    applicationContext,
+                    exception.localizedMessage.toString(),
+                    Toast.LENGTH_LONG
+                ).show()
+
+            } else {
+                if (snapshot != null) {
+                    if (!snapshot.isEmpty) {
+                        val documents = snapshot.documents
+                        for (document in documents) {
+                            val comment = document.get("comment") as String
+                            val userEmail = document.get("userEmail") as String
+                            val downloadUrl = document.get("downloadUrl") as String
+                            val timestamp = document.get("date") as Timestamp
+                            val date = timestamp.toDate()
+
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
 }
