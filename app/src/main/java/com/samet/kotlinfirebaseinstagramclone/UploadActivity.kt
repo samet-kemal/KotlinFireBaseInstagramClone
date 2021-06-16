d345ffc6a405f76f7734f79af9d02d2575c1baab
samet-kemal <77715935+samet-kemal@users.noreply.github.com>
2021-06-16 20:22:01 +0300

    OnClick Methods Added

--- app/src/main/java/com/samet/kotlinfirebaseinstagramclone/UploadActivity.kt
+++ app/src/main/java/com/samet/kotlinfirebaseinstagramclone/UploadActivity.kt
@@ -1,11 +1,20 @@
 package com.samet.kotlinfirebaseinstagramclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class UploadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload)
+    }
+
+    fun uploadClicked(view: View) {
+
+    }
+
+    fun imageViewClicked(view: View) {
+
     }
 }
